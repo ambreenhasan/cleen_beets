0953eba5e7153c0f61d7bed562d883013ecb63cf
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-28 15:12:30 -0800

    still working on parsing the results

--- app/models/itunes.rb
+++ app/models/itunes.rb
@@ -3,18 +3,31 @@ require 'Date'
 
 module ITunes
   class API
-    attr_reader :results, :artist_name, :collection_name, :track_censored_name, :release_year, :primary_genre_name, :track_number
+    attr_reader :results, :parsed, :artist_name, :collection_name, :track_censored_name, :release_year, :primary_genre_name, :track_number
     def self.search(searchable)
       @results = ITunesSearchAPI.search(:term => searchable, :country => "US", :media => "music")
     end
 
     def self.parse_results
-      @results.each do |track|
-        p "#{track["artistName"]} - #{track["collectionName"]} - #{track["trackCensoredName"]}"
-        puts Date._strptime(track["releaseDate"])[:year]
-        puts track["primaryGenreName"]
-        puts track["trackNumber"]
+      @parsed = @results.inject({}) do |track, result|
+        # @parsed = {:artist_name => track["artistName"], :collection_name => track["collectionName"], :track_censored_name => track["trackCensoredName"], :release_year => Date._strptime(track["releaseDate"])[:year], :primary_genre_name => track["primaryGenreName"], :track_number => track["trackNumber"]}
+
+        # p track_info
+        # p result[:]
+
+        # p track_infoartist_name]
+
+        track[:artist_name] = result["artistName"]
+        # @parsed[:collection_name] = track_info["collectionName"]
+        # @parsed[:track_censored_name] = track_info["trackCensoredName"]
+        # @artist_name = track["artistName"]
+        # p "#{track["artistName"]} - #{track["collectionName"]} - #{track["trackCensoredName"]}"
+        # puts
+        # puts track["primaryGenreName"]
+        # puts track["trackNumber"]
       end
+
+      puts @parsed
     end
   end
 end
--- driver.rb
+++ driver.rb
@@ -1,3 +1,4 @@
 require_relative 'config/application'
 
 ITunes::API.search("henry saiz haunted girl canyon")
+ITunes::API.parse_results
