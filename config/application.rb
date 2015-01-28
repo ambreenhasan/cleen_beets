3345787357feaf8689bea0d7bbc023ade81d0225
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-28 12:23:09 -0800

    added configurations for application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,2 +1,15 @@
+require 'pathname'
+require 'logger'
+
 require 'dotenv'
 Dotenv.load
+
+APP_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
+
+APP_NAME = APP_ROOT.basename.to_s
+
+DB_PATH  = APP_ROOT.join('db', APP_NAME + ".db").to_s
+
+if ENV['DEBUG']
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+end
