81f8f2bc4dc49300d0ca9c338b992130767a18b2
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-29 13:07:46 -0800

    properly getting an array of hashes for itunes results

--- app/models/itunes.rb
+++ app/models/itunes.rb
@@ -3,31 +3,19 @@ require 'Date'
 
 module ITunes
   class API
-    attr_reader :results, :parsed, :artist_name, :collection_name, :track_censored_name, :release_year, :primary_genre_name, :track_number
+    attr_reader :results, :parsed
     def self.search(searchable)
       @results = ITunesSearchAPI.search(:term => searchable, :country => "US", :media => "music")
     end
 
     def self.parse_results
-      @parsed = @results.inject({}) do |track, result|
-        # @parsed = {:artist_name => track["artistName"], :collection_name => track["collectionName"], :track_censored_name => track["trackCensoredName"], :release_year => Date._strptime(track["releaseDate"])[:year], :primary_genre_name => track["primaryGenreName"], :track_number => track["trackNumber"]}
-
-        # p track_info
-        # p result[:]
-
-        # p track_infoartist_name]
-
-        track[:artist_name] = result["artistName"]
-        # @parsed[:collection_name] = track_info["collectionName"]
-        # @parsed[:track_censored_name] = track_info["trackCensoredName"]
-        # @artist_name = track["artistName"]
-        # p "#{track["artistName"]} - #{track["collectionName"]} - #{track["trackCensoredName"]}"
-        # puts
-        # puts track["primaryGenreName"]
-        # puts track["trackNumber"]
+      @parsed = @results.map do |track|
+        {:artist_name => track["artistName"], :collection_name => track["collectionName"], :track_censored_name => track["trackCensoredName"], :release_year => Date._strptime(track["releaseDate"])[:year], :primary_genre_name => track["primaryGenreName"], :track_number => track["trackNumber"]}
       end
 
-      puts @parsed
+      p track
+
+      p @parsed
     end
   end
 end
--- driver.rb
+++ driver.rb
@@ -1,5 +1,5 @@
 require_relative 'config/application'
 
-# ITunes::API.search("henry saiz haunted girl canyon")
-# ITunes::API.parse_results
-ID3Tag::Editor.write_tags(ARGV[0])
+ITunes::API.search("henry saiz haunted girl canyon")
+ITunes::API.parse_results
+# ID3Tag::Editor.write_tags(ARGV[0])
