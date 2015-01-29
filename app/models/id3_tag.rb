35a8e5351785108aad62827322934bd45c6f4322
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-28 21:30:30 -0800

    files are successfully saved and renamed in current directory

--- app/models/id3_tag.rb
+++ app/models/id3_tag.rb
@@ -1,28 +1,34 @@
 require 'taglib'
 
+# TODO: refactor the hell out of this
+require 'pathname'
+
 module ID3Tag
   class Editor
-    attr_accessor :artist, :album, :title
     def self.write_tags(mp3_file)
       TagLib::MPEG::File.open(mp3_file) do |file|
 
         tag = file.id3v2_tag
         puts "Tags are empty." if tag.title.nil?
 
-        tag.title = "Atlantique"
+        tag.title = "Pacifique"
         tag.artist = "Damabiah"
-        tag.track = 1
+        tag.track = 2
         tag.album = "Océans - Single"
         tag.year = 2015
-        tag.genre = "Tech House"
+        tag.genre = "Indie House"
         tag.comment = " "
 
-        File.rename(mp3_file, "#{tag.artist} - #{tag.album} - #{tag.title}.mp3")
+          pn = Pathname.new(mp3_file)
+
+        File.rename(mp3_file, "#{pn.dirname}/#{tag.artist} - #{tag.album} - #{tag.title}.mp3")
 
         file.save
 
         puts "File has been saved and renamed."
       end
+
+
     end
   end
 end
