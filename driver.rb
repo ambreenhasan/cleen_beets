728cadc41af416cc3113984a9ba8e5be2a9783fe
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-28 21:12:56 -0800

    deleted music model

--- driver.rb
+++ driver.rb
@@ -3,5 +3,3 @@ require_relative 'config/application'
 # ITunes::API.search("henry saiz haunted girl canyon")
 # ITunes::API.parse_results
 ID3Tag::Editor.write_tags(ARGV[0])
-# MusicFile::Editor.rename_file(ARGV[0])
-# MusicFile::Editor.new_file
