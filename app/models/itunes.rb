eac2076e350b9d56a7f12fedd8fbe615276c212c
Ambreen Hasan <ambodumbo@gmail.com>
2015-01-29 15:54:12 -0800

    still working on parsing logic

--- app/models/itunes.rb
+++ app/models/itunes.rb
@@ -1,24 +1,31 @@
 require 'itunes-search-api'
+require 'pathname'
 require 'Date'
 
 module ITunes
   class API
     attr_reader :results, :parsed
     def self.search(searchable)
-      @results = ITunesSearchAPI.search(:term => searchable, :country => "US", :media => "music")
+      parse_search(searchable)
+      # @results = ITunesSearchAPI.search(:term => searchable, :country => "US", :media => "music")
+    end
+
+    def self.parse_search(directory_path)
+      file_base = File.basename(directory_path)
+      cleaned = file_base.gsub(/[\.,-\/#$%\^&\*;:{}=\-_`~()]/, "")
+      # compare with id3 tags
+      cleaned.split(" ").find do |ele|
+        if ele == ele
+          p ele
+        end
+      end
     end
 
     def self.parse_results
-      @parsed = @results.map do |track|
-        {:artist_name => track["artistName"], :collection_name => track["collectionName"], :track_censored_name => track["trackCensoredName"], :release_year => Date._strptime(track["releaseDate"])[:year], :primary_genre_name => track["primaryGenreName"], :track_number => track["trackNumber"]}
-      end
-
-      p track
-
-      p @parsed
+      # @parsed = @results.map do |track|
+      #   {:artist_name => track["artistName"], :collection_name => track["collectionName"], :track_censored_name => track["trackCensoredName"], :release_year => Date._strptime(track["releaseDate"])[:year], :primary_genre_name => track["primaryGenreName"], :track_number => track["trackNumber"]}
+      # end
     end
   end
 end
-
-# artistName, collectionName, trackCensoredName, releaseDate, primaryGenreName, trackNumber
 
