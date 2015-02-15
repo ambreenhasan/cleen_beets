cc2c65b64ded1e6f56fb875b014ebf52ba7a6095
Ambreen Hasan <ambodumbo@gmail.com>
2015-02-15 15:58:46 -0800

    still working on junodownload scraper

--- app/models/junodownload.rb
+++ app/models/junodownload.rb
@@ -3,13 +3,14 @@ require 'pp'
 
 module JunoDownload
   class Scraper
+    attr_accessor :product_information
 
     def self.page
       agent = Mechanize.new
       return agent.get('http://junodownload.com')
     end
 
-    def self.search(song)
+    def self.scrape(song)
       juno_search = page.form
       search_field = juno_search.field_with(name: "q[all][]")
 
@@ -18,14 +19,12 @@ module JunoDownload
       home_page = juno_search.submit
 
       tracks_page = home_page.link_with(:text => "\r\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tTracks\r\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t").click
-
-      pp tracks_page
     end
   end
 end
 
 
-pp JunoDownload::Scraper.search("solee phoenix johannes brecht remix")
+# pp JunoDownload::Scraper.scrape("solee phoenix johannes brecht remix")
 
 
 
