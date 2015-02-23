21e3c5ef0dc768300b64a7a19d27b973f4e64a93
Ambreen Hasan <ambodumbo@gmail.com>
2015-02-23 13:31:53 -0800

    working through beatport parser

--- app/models/beatport.rb
+++ app/models/beatport.rb
@@ -1,6 +1,29 @@
+require 'mechanize'
+require 'json'
+require 'pp'
+
+
 module Beatport
   class Scraper
-    def self.search(song)
+
+    def self.submit_search(song)
+      agent = Mechanize.new
+      page = agent.get("http://classic.beatport.com")
+
+      beatport_search_form = page.form('global-search-form')
+      beatport_search_form.query = song
+
+      page = agent.submit(beatport_search_form, beatport_search_form.buttons.first)
+
+      page = agent.page.link_with(:text => 'Estroe, Pawas - Jujeh').click
+
+      page.search('#body > div.line.release-detail > div.unit.lastUnit > div:nth-child(5) > table').each do |track|
+        track.children.first.attributes.each do |parsable|
+          pp parsable
+        end
+      end
+
+      @results = []
     end
 
     def self.parse_search(mp3_path)
@@ -10,3 +33,5 @@ module Beatport
     end
   end
 end
+
+Beatport::Scraper.submit_search("pawas jujeh estroe remix")
